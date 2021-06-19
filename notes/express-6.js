7e1a42f14eb33862cde36f9583d977c472c635cf
toastDerivation <jmgnngmj@gmail.com>
2021-06-19 11:17:04 +0800

    61921

--- notes/express-6.js
+++ notes/express-6.js
@@ -30,6 +30,44 @@ app.post('/api/people',(req,res)=>{
     res.status(201).json({success:true,person:name})
 })
 
+app.post('/api/postman/people',(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success : false, msg:'Please provide name value'})
+    }
+    res.status(201).json({success:true,data:[...people, name]})
+})
+
+app.put('/api/people/:id',(req,res)=>{
+    const {id} = req.params
+    const {name} = req.body
+
+    const person = people.find((elem) => elem.id === Number(id))
+
+    if(!person){
+        return res.status(404).json({success : false, msg:'cant find person id'})
+    }
+
+    const newPeople = people.map((elem) =>{
+        if(elem.id === Number(id)){
+            elem.name = name
+        }
+        return elem
+    })
+    res.status(200).json({success:true, data : newPeople})
+})
+
+app.delete('/api/people/:id',(req,res)=>{
+    const person = people.find((elem) => elem.id === Number(req.params.id))
+
+    if(!person){
+        return res.status(404).json({success : false, msg:'cant find person id'})
+    }
+
+    const newPeople = people.filter((elem) => elem.id !== Number(req.params.id))
+    return res.status(200).json({success:true, data: newPeople})
+})
+
 app.listen(3000, ()=>{
     console.log('Listening to port 3000...')
 })
