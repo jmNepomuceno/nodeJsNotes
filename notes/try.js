06cb0f0785dd03bf4ac6c4f3cddaa8852acea49a
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2021-06-19 22:32:35 +0800

    61921_b

--- notes/try.js
+++ notes/try.js
@@ -1,5 +1,7 @@
-const {ex} = require('./database')
-
-let arr = [...ex]
-
-console.log(arr)
+const http = require('http')
+http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.write(JSON.stringify([1,2,3]))
+        res.end()
+    }
+}).listen(3000,()=> console.log("Listening to port 3000..."))
